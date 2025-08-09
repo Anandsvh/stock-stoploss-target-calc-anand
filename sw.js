ffc57ff5bf609263c069e37564451da7b84e6219
Anandsvh <anandsvh@gmail.com>
2025-08-09 21:50:28 +0530

    updated the serives worker

--- sw.js
+++ sw.js
@@ -1,16 +1,42 @@
+const CACHE_NAME = "avk-stoploss-cache-v1.1"; // change version when updating
+
+const FILES_TO_CACHE = [
+  "/",
+  "/index.html",
+  "/manifest.json",
+  "/icon-192.png",
+  "/icon-512.png",
+  "/style.css",
+  "/script.js"
+];
+
+// Install
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open("sl-target-cache").then(cache => {
-      return cache.addAll([
-        "./index.html",
-        "./manifest.json",
-        "./icon-192.png",
-        "./icon-512.png"
-      ]);
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(FILES_TO_CACHE);
     })
   );
+  self.skipWaiting();
 });
 
+// Activate
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+// Fetch
 self.addEventListener("fetch", event => {
   event.respondWith(
     caches.match(event.request).then(response => {
